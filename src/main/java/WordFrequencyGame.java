466a42396f445d55979723df5514a5d01292126a
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-09 20:06:47 +0800

    [WordFrequencyCounterExercise][Gerarld] refactor : remove unused codes & methods

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -29,30 +29,6 @@ public class WordFrequencyGame {
     }
 
     private List<WordInfo> calculateWordCount(String inputString) {
-        //split the input string with 1 to n pieces of spaces
-        String[] wordList = inputString.split(BLANK_SPACES);
-
-        List<WordInfo> wordInfoList = new ArrayList<>();
-        for (String s : wordList) {
-            WordInfo wordInfo = new WordInfo(s, 1);
-            wordInfoList.add(wordInfo);
-        }
-
-        //get the map for the next step of sizing the same word
-        Map<String, List<WordInfo>> map = getListMap(wordInfoList);
-
-        List<WordInfo> list = new ArrayList<>();
-        for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
-            WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-            list.add(wordInfo);
-        }
-        wordInfoList = list;
-
-        wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-        return wordInfoList;
-    }
-
-    private List<WordInfo> calculateWordCount_Temp(String inputString) {
         List<String>words = Arrays.asList(inputString.split(BLANK_SPACES));
         List<String> distinctWords = words.stream().distinct().collect(Collectors.toList());
         List<WordInfo> wordInfos = new ArrayList<>();
@@ -64,20 +40,5 @@ public class WordFrequencyGame {
         }
         wordInfos.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
         return wordInfos;
-    }
-
-    private Map<String, List<WordInfo>> getListMap(List<WordInfo> wordInfoList) {
-        Map<String, List<WordInfo>> map = new HashMap<>();
-        for (WordInfo wordInfo : wordInfoList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(wordInfo.getValue())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordInfo);
-                map.put(wordInfo.getValue(), arr);
-            } else {
-                map.get(wordInfo.getValue()).add(wordInfo);
-            }
-        }
-        return map;
     }
 }
