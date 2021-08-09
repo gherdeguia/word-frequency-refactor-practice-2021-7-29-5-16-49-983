3ec1c6b8ac726fa47989bed31fc6c43717cacfd7
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-09 19:38:10 +0800

    [WordFrequencyCounterExercise][Gerarld] refactor : extracted code to calculateWordCount method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -6,31 +6,12 @@ public class WordFrequencyGame {
 
     public String getResult(String inputString) {
         if (inputString.split(BLANK_SPACES).length == 1) {
-            return inputString + BLANK_SPACES;
+            return inputString + " 1";
         } else {
 
             try {
 
-                //split the input string with 1 to n pieces of spaces
-                String[] wordList = inputString.split(BLANK_SPACES);
-
-                List<WordInfo> wordInfoList = new ArrayList<>();
-                for (String s : wordList) {
-                    WordInfo wordInfo = new WordInfo(s, 1);
-                    wordInfoList.add(wordInfo);
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<WordInfo>> map = getListMap(wordInfoList);
-
-                List<WordInfo> list = new ArrayList<>();
-                for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
-                    WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-                    list.add(wordInfo);
-                }
-                wordInfoList = list;
-
-                wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                List<WordInfo> wordInfoList = calculateWordCount(inputString);
 
                 StringJoiner joiner = new StringJoiner("\n");
                 for (WordInfo w : wordInfoList) {
@@ -44,6 +25,30 @@ public class WordFrequencyGame {
                 return "Calculate Error";
             }
         }
+    }
+
+    private List<WordInfo> calculateWordCount(String inputString) {
+        //split the input string with 1 to n pieces of spaces
+        String[] wordList = inputString.split(BLANK_SPACES);
+
+        List<WordInfo> wordInfoList = new ArrayList<>();
+        for (String s : wordList) {
+            WordInfo wordInfo = new WordInfo(s, 1);
+            wordInfoList.add(wordInfo);
+        }
+
+        //get the map for the next step of sizing the same word
+        Map<String, List<WordInfo>> map = getListMap(wordInfoList);
+
+        List<WordInfo> list = new ArrayList<>();
+        for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
+            WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
+            list.add(wordInfo);
+        }
+        wordInfoList = list;
+
+        wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+        return wordInfoList;
     }
 
 
