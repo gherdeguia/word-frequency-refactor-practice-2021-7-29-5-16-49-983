2ac4d207e632721441ea024ab01def42588aa44e
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-09 15:31:59 +0800

    [WordFrequencyCounterExercise][Gerarld] refactor : renamed input class to WordInfo & renamed WordInfo attributes for more readability

---
+++ src/main/java/WordInfo.java
@@ -0,0 +1,17 @@
+public class WordInfo {
+    private final String word;
+    private final int wordCount;
+
+    public WordInfo(String word, int wordCount) {
+        this.word = word;
+        this.wordCount = wordCount;
+    }
+
+    public String getValue() {
+        return this.word;
+    }
+
+    public int getWordCount() {
+        return this.wordCount;
+    }
+}
